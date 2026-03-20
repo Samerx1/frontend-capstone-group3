9fc167ee74485c20f66db94930801575807c5b2f
jerrybarry <barnabasjeremiah01@gmail.com>
2026-03-20 22:46:53 +0100

    init: setup environment

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,25 @@
+import './Header.css'
+
+function Header() {
+  return (
+    <header className="header">
+      <div className="header-inner">
+        {/*
+          Logo from Figma:
+          width: 121px, height: 52px
+          left: 119px, top: 27px
+          Replace src with your actual logo image file
+        */}
+        <div className="header-logo">
+          <img
+            src="/planet-logo.png"
+            alt="Planet Creative World"
+            className="header-logo-img"
+          />
+        </div>
+      </div>
+    </header>
+  )
+}
+
+export default Header
